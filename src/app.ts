1418308f977e8ac2bc72a1ef5582aa924b08c592
Arman Hossain Atunu <armanhossainatunu@gmail.com>
2026-06-22 18:06:23 +0600

    lib file and some file creation

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
-import express, { Application } from "express"
+import express, { Application, Request, Response } from "express"
 
 const app : Application = express();
-
+app.get("/", (req : Request, res : Response) => {
+    res.send("Hello World");
+});
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import app from "./app";
+import { prisma } from "./lib/prisma";
 
 async function main() {
   const port = process.env.PORT || 5000;
   try {
+    await prisma.$connect();
+    console.log("Connected to the database successfully.");
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
   } catch (error) {
     console.error("Error starting server:", error);
+    await prisma.$disconnect();
     process.exit(1);
   }
 }
