f4b60ca91588ed9094e09598136d095975bf579b
Arman Hossain Atunu <armanhossainatunu@gmail.com>
2026-06-23 16:56:26 +0600

    database connected done

--- src/server.ts
+++ src/server.ts
@@ -5,14 +5,14 @@ import { prisma } from "./lib/prisma";
 async function main() {
   const port = config.PORT;
   try {
-    // await prisma.$connect();
+    await prisma.$connect();
     console.log("Connected to the database successfully.");
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
   } catch (error) {
     console.error("Error starting server:", error);
-    // await prisma.$disconnect();
+    await prisma.$disconnect();
     process.exit(1);
   }
 }
